d2cdbbe7088acca0f29d58ffbac0ce09c974cdda
HueyWhyte <stanleykwaminaotabil@gmail.com>
2023-01-07 11:58:03 +0000

    from slygh to tinyBlogger

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -3,6 +3,7 @@ import { NavLink, Link } from "@remix-run/react";
 const links = [
   { name: "Home", href: "/" },
   { name: "Browse", href: "/blog" },
+  // { name: "Login", href: "/login" },
   // { name: "Blog", href: "/blog" },
 ];
 
@@ -10,7 +11,7 @@ function NavigationBar() {
   return (
     <nav className="fixed z-50 flex h-20 w-full items-center border-b border-black bg-slate-50 px-3">
       <Link to="/" className="logo text-2xl font-black">
-        Sly GH
+        tiny Blogger
       </Link>
 
       <div className="ml-auto flex items-center">
@@ -28,12 +29,12 @@ function NavigationBar() {
           </NavLink>
         ))}
 
-        {/* <Link
+        <Link
           to="/auth"
           className="mx-3 mr-auto rounded-2xl bg-black py-2 px-5 font-medium text-white shadow-md shadow-gray-400 ring-black transition-all duration-75 hover:bg-yellow-400 hover:text-black hover:ring-2"
         >
           Sign In
-        </Link> */}
+        </Link>
       </div>
     </nav>
   );
