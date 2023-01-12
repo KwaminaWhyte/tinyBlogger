eb583fe0b15aea8dd258a4729c516c4f5c0c79fa
HueyWhyte <stanleykwaminaotabil@gmail.com>
2023-01-12 02:39:02 +0000

    nav done

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -44,49 +44,49 @@ function NavigationBar({
             to={link.href}
             className={({ isActive }) =>
               isActive
-                ? "ml-1 mr-1 decoration-slice font-semibold text-black underline underline-offset-8"
-                : "ml-1 mr-1"
+                ? "ml-1 mr-1 decoration-slice text-sm font-semibold text-black underline underline-offset-8"
+                : "ml-1 mr-1 text-sm"
             }
           >
             {link.name}
           </NavLink>
         ))}
 
-        <Dropdown>
-          <Dropdown.Trigger>
-            <span className="inline-flex rounded-md">
-              <button
-                type="button"
-                className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
-              >
-                something
-                <svg
-                  className="ml-2 -mr-0.5 h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </span>
-          </Dropdown.Trigger>
-
-          <Dropdown.Content>
-            <Dropdown.Link href="create/blog/new" label="New Blog" />
-            <Dropdown.Link href="user/profile" label="Profile" />
-          </Dropdown.Content>
-        </Dropdown>
-
         {isAuthenticated ? (
-          <div className="relative">
+          <div className="relative flex">
+            <Dropdown>
+              <Dropdown.Trigger>
+                <span className="inline-flex rounded-md">
+                  <button
+                    type="button"
+                    className="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                  >
+                    More
+                    <svg
+                      className="ml-2 -mr-0.5 h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </span>
+              </Dropdown.Trigger>
+
+              <Dropdown.Content>
+                <Dropdown.Link href="create/blog/new" label="New Blog" />
+                <Dropdown.Link href="user/profile" label="Profile" />
+              </Dropdown.Content>
+            </Dropdown>
+
             <button
               ref={buttonRef}
-              className="ml-3 flex items-center rounded-full bg-gray-200 px-2 py-1 transition-all duration-300 hover:bg-gray-300"
+              className="ml-3 flex items-center rounded-full bg-gray-200 px-2 py-1 text-sm transition-all duration-300 hover:bg-gray-300"
               onClick={toggleDropdown}
             >
               <p>{user?.email}</p>
@@ -103,10 +103,7 @@ function NavigationBar({
               >
                 <li className="px-4 py-1 hover:bg-gray-100">
                   <Form className="block w-full px-4 py-1" method="post">
-                    <button
-                      className="w-full text-left text-red-600"
-                      // onClick={toggleDropdown}
-                    >
+                    <button className="w-full text-left text-red-600">
                       Logout
                     </button>
                   </Form>
